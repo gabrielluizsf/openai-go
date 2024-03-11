90af603bd1b0e885ce0339ac1b78ff6a71e4e080
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-11 14:51:39 -0300

    implementing the OpenAIClient interface

---
+++ pkg/openai/tts_with_context.go
@@ -0,0 +1,42 @@
+package openai
+
+import (
+	"net/http"
+
+	"github.com/gabrielluizsf/openai-go/internal/request"
+)
+
+func (oc *ClientWithContext) TextToSpeech(model, input, voice string) (*TTSResult, error) {
+	url := BASE_URL + "/audio/speech"
+	openaiAPIKey := oc.APIKey
+	if openaiAPIKey == "" {
+		return nil, InvalidAPIKey()
+	}
+
+	requestBody, err := request.MarshalJSON(request.JSON{
+		"model": model,
+		"input": input,
+		"voice": voice,
+	})
+	if err != nil {
+		return nil, CreateBodyError(err)
+	}
+
+	req, err := request.WithContext(oc.Ctx, http.MethodPost, url, requestBody)
+	if err != nil {
+		return nil, CreateRequestError(err)
+	}
+	setHeaders(req, oc, "application/json")
+	client := request.Client()
+	resp, err := request.Response(req, client)
+	if err != nil {
+		return nil, SendRequestError(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, RequestError(resp.StatusCode)
+	}
+	return &TTSResult{
+		Audio: resp.Body,
+	}, nil
+}
---
+++ pkg/openai/whisper_whith_context.go
@@ -0,0 +1,62 @@
+package openai
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+
+	"github.com/gabrielluizsf/openai-go/internal/request"
+)
+
+// model https://platform.openai.com/docs/models/model-endpoint-compatibility
+func (oc *ClientWithContext) AudioTranscription(model, fileName, audioFilePath string) (*AudioTranscriptionResponse, error) {
+	file, err := os.Open(audioFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	part, err := writer.CreateFormFile("file", fileName)
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return nil, err
+	}
+
+	writer.WriteField("model", model)
+
+	writer.Close()
+
+	url := BASE_URL + "/audio/transcriptions"
+	req, err := request.NewWithContextBuffer(oc.Ctx, url, http.MethodPost, body)
+	if err != nil {
+		return nil, err
+	}
+	setHeaders(req, oc, writer.FormDataContentType())
+
+	client := request.Client()
+	resp, err := request.Response(req, client)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result AudioTranscriptionResponse
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, RequestError(resp.StatusCode)
+	}
+	return &result, nil
+
+}
