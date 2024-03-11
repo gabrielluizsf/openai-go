0671b1e3af4e58b537ee6ba39b7100da5d83724a
gabrielluizsf <gabrielluizads123@gmail.com>
2024-03-11 13:48:48 -0300

    examples

---
+++ example/audio/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/gabrielluizsf/openai-go/pkg/openai"
+)
+
+func SaveTextToSpeech(client *openai.Client, model, input, voice, filePath string) error {
+	ttsResult, err := client.TextToSpeech(model, input, voice)
+	if err != nil {
+		return err
+	}
+	defer ttsResult.Audio.Close()
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, ttsResult.Audio)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Áudio salvo com sucesso em: %s\n", filePath)
+	return nil
+}
+
+func main() {
+	openai := openai.New(os.Getenv("OPENAI_KEY"))
+	message := "oi"
+	n, err := rand.Read([]byte(message))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileName := fmt.Sprintf("%d%s.mp3", n, message)
+	filePath := "./temp/" + fileName
+	if err := SaveTextToSpeech(openai, "tts-1", message, "onyx", filePath); err != nil {
+		log.Fatal(err)
+	}
+	transcription, err := openai.AudioTranscription("whisper-1", fileName, filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf(transcription.Text)
+}
---
+++ example/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gabrielluizsf/openai-go/pkg/openai"
+	"github.com/gabrielluizsf/openai-go/pkg/openai/chat"
+)
+
+func main() {
+	openaiAPIKey := os.Getenv("OPENAI_KEY")
+
+	openai := openai.New(openaiAPIKey)
+	model := "gpt-3.5-turbo"
+	chatCompletion, err := openai.ChatGPT(
+		model,
+		[]chat.Message{
+			{
+				Role:    "system",
+				Content: "You are a helpful assistant.",
+			},
+			{
+				Role:    "user",
+				Content: "oie",
+			},
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID: %s\n", chatCompletion.ID)
+	fmt.Printf("Object: %s\n", chatCompletion.Object)
+	fmt.Printf("Created: %d\n", chatCompletion.Created)
+	fmt.Printf("Model: %s\n", chatCompletion.Model)
+	fmt.Printf("System Fingerprint: %s\n", chatCompletion.SystemFingerprint)
+	for _, choice := range chatCompletion.Choices {
+		fmt.Printf("Choice Index: %d\n", choice.Index)
+		fmt.Printf("Choice Finish Reason: %s\n", choice.FinishReason)
+		fmt.Printf("Choice Message Role: %s\n", choice.Message.Role)
+		fmt.Printf("Choice Message Content: %s\n", choice.Message.Content)
+		if choice.Logprobs != nil {
+			fmt.Printf("Choice Logprobs: %s\n", *choice.Logprobs)
+		}
+	}
+	fmt.Printf("Prompt Tokens: %d\n", chatCompletion.Usage.PromptTokens)
+	fmt.Printf("Completion Tokens: %d\n", chatCompletion.Usage.CompletionTokens)
+	fmt.Printf("Total Tokens: %d\n", chatCompletion.Usage.TotalTokens)
+}
